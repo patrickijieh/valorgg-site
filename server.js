5d99a11d70bed4c185484ed57e38ab7a4bb679d2
Patrick Ijieh <123503029+patrickijieh@users.noreply.github.com>
2023-06-23 19:00:58 -0400

    server file

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+app.use(express.json());
+
+// valorant api routes
+const valorantRouter = require('./routes/valorantapi');
+app.use('/valorantapi', valorantRouter);
+
+// user routes
+const accountRouter = require('./routes/users');
+app.use('/users', accountRouter);
+
+// open server
+app.listen(3000, () => console.log('Server started'));
